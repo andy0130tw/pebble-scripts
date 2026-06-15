9e8205b167fbcca1ed0753b650b1417aa43d0f25
Andy Pan <andy0130tw@yahoo.com.tw>
2026-06-16 04:45:49 +0800

    Add back offset; not knowing why cubic11 still needs it

--- render_fonts.py
+++ render_fonts.py
@@ -5,25 +5,26 @@ from fontgen import Font
 MAX_GLYPHS_EXTENDED = 255 * 128
 MAX_GLYPH_SIZE = 512
 
-PbfSpec = namedtuple('PbfSpec', 'name maxh pxsize inp cpsrc bold')
+PbfSpec = namedtuple('PbfSpec', 'name maxh pxsize offs inp cpsrc bold')
 
 specs = [
-    PbfSpec('cubic-14.pbf',   14, 12, 'Cubic_11.ttf',             'cubic11-cps.json', False),
-    PbfSpec('cubic-18.pbf',   18, 12, 'Cubic_11.ttf',             'cubic11-cps.json', False),
+    PbfSpec('cubic-14.pbf',   14, 12,  0, 'Cubic_11.ttf',             'cubic11-cps.json', False),
+    PbfSpec('cubic-18.pbf',   18, 12, -2, 'Cubic_11.ttf',             'cubic11-cps.json', False),
 
-    PbfSpec('sarasa-24.pbf',  24, 19, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    False),
-    PbfSpec('sarasa-24b.pbf', 24, 19, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    True),
-    PbfSpec('sarasa-28.pbf',  28, 21, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    False),
-    PbfSpec('sarasa-28b.pbf', 28, 21, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    True),
+    PbfSpec('sarasa-24.pbf',  24, 19,  0, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    False),
+    PbfSpec('sarasa-24b.pbf', 24, 19,  0, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    True),
+    PbfSpec('sarasa-28.pbf',  28, 21,  0, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    False),
+    PbfSpec('sarasa-28b.pbf', 28, 21,  0, 'SarasaGothicTC-Light.ttf', 'cps-kuro.json',    True),
 ]
 
 for spec in specs:
     print('spec', spec)
-    name, maxh, pxsize, inp, cpsrc, bb = spec
+    name, maxh, pxsize, offs, inp, cpsrc, bb = spec
 
     f = Font(inp, pxsize, MAX_GLYPHS_EXTENDED, MAX_GLYPH_SIZE, False, max_height=maxh)
 
     f.set_codepoint_list(cpsrc)
+    f.set_heightoffset(offs)
     if bb:
         f.set_fauxbold(True)
 
